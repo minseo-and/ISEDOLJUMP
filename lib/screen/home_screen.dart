a9a98c7b58c25ff1526168e40c6e622f3d53ad48
minseo-and <msp05070478@gmail.com>
2023-03-13 11:05:37 +0900

    움직임 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:sedol_jump/entity/player.dart';
 class HomeScreen extends StatefulWidget {
@@ -10,10 +12,30 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   double playerYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = 0;
+  bool gameHasStarted = false;
 
   void jump() {
     setState(() {
-      playerYaxis -= 0.1;
+      time = 0;
+      initialHeight = playerYaxis;
+    });
+  }
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      time += 0.04;
+      height = -4.9 * time * time + 2.8 * time;
+      setState(() {
+        playerYaxis = initialHeight - height;
+      });
+      if (playerYaxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
     });
   }
 
@@ -25,7 +47,13 @@ class _HomeScreenState extends State<HomeScreen> {
           Expanded(
             flex: 2,
             child: GestureDetector(
-              onTap: jump,
+              onTap: () {
+                if(gameHasStarted) {
+                  jump();
+                } else {
+                  startGame();
+                }
+              },
               child: AnimatedContainer(
                 alignment: Alignment(0, playerYaxis),
               duration: Duration(milliseconds: 0),
@@ -34,12 +62,39 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
+          Container(
+            height: 20,
+            color: Colors.blue,
+          ),
           Expanded(child: Container(
             color: Color(0xFFFFF8BE),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                score('SCORE', 0),
+                score('BEST', 10),
+              ],
+            ),
           ),
           ),
         ],
       )
     );
+  }
+
+  Column score( String name, int score) {
+    return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(name, style: TextStyle(
+                    color: Colors.pinkAccent, fontSize: 20
+                  ),),
+                  SizedBox(height: 20),
+                  Text('$score', style: TextStyle(
+                      color: Colors.lightBlue, fontSize: 35,
+                    fontWeight: FontWeight.bold
+                  ),)
+                ],
+              );
   }
 }
